163c8c9841eada06c00495ef650160ebb4a01efc
Leo <leo.a.alho@gmail.com>
2023-01-08 12:02:41 +0200

    Fixing eslint v2

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,10 +16,7 @@ module.exports = {
             'error',
             4
         ],
-        'linebreak-style': [
-            'error',
-            'windows'
-        ],
+        'linebreak-style': 0,
         'quotes': [
             'error',
             'single'
